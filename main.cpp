d7bee5bc5b9cd3b7aaa11795fe16b23af3f0c7e0
Constantinos Vafias <cv2134@nyu.edu>
2026-02-25 16:29:13 -0500

    Changing values for faster runtime

--- main.cpp
+++ main.cpp
@@ -102,8 +102,8 @@ int main() {
 
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_width       = 400;
-    cam.samples_per_pixel = 100;
-    cam.max_depth         = 50;
+    cam.samples_per_pixel = 50;
+    cam.max_depth         = 5;
 
     cam.vfov     = 20;
     cam.lookfrom = point3(13,2,3);
